b4fd787270a5983dfdc34a225c84db3355557f61
nitesh <niteshkr.gupta787@gmail.com>
2023-08-05 13:58:29 +0530

    UpdatedV2

--- GitPractices/src/main/java/Test.java
+++ GitPractices/src/main/java/Test.java
@@ -1,6 +1,7 @@
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("Self First Git program commit");
-	       Syst(dnkjc);
+	       //Syst(dnkjc);
+		System.out.println("Update program here version2.0");
 	}
 }
